38c3ebdaedc2927836a5c6e36e3626e361dbb6e8
Luana Moreira <luanamoreirafm@gmail.com>
2024-07-04 17:18:53 -0300

    Update index.php

--- view/index.php
+++ view/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Satisfação Alimentarr</title>
+    <title>Satisfação Alimentar</title>
     <link rel="shortcut icon" href="https://icons.iconarchive.com/icons/aha-soft/desktop-buffet/48/Steak-icon.png" type="png">
     <link rel="stylesheet" href="css/style.css">
 </head>
