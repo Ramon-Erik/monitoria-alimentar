7f76874cd713e28518daac157244b4f6fbacf111
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2024-07-22 16:04:44 -0300

    atualizando o cardápio do lanche

--- model/Cardapio.Class.php
+++ model/Cardapio.Class.php
@@ -8,9 +8,21 @@ Class Cardapio {
 
     public function get_cardapio($data, $horario) {
         try {
-            $select = $this->pdo->prepare("SELECT id FROM cardapio WHERE data = :data and tipo_refeicao = :tipo");
+            $select = $this->pdo->prepare("SELECT * FROM cardapio WHERE data = :data and tipo_refeicao = :tipo");
             $select->bindValue(":data", $data); 
             $select->bindValue(":tipo", $horario); 
+            $select->execute();
+            $select = $select->fetch(PDO::FETCH_ASSOC);
+            return $select;
+        } catch (Exception $e) {
+            echo $e->getCode();
+        }
+    }
+    
+    public function get_cardapio_servido($id) {
+        try {
+            $select = $this->pdo->prepare("SELECT * FROM cardapio_servido WHERE id = :id");
+            $select->bindValue(":id", $id); 
             $select->execute();
             $select = $select->fetch(PDO::FETCH_ASSOC);
             return $select;
@@ -39,6 +51,28 @@ Class Cardapio {
         }
     }
 
+    public function count_null($array) {
+        $quantidade = 0;
+        foreach ($array as $value) {
+            if ($value === NULL) {
+                $quantidade++;
+            }
+        }
+        return $quantidade;
+    }
+
+    public function atualizar_cardapio_lanche($id, $ref_solida, $ref_liquida) {
+        try {
+            $cardapio_servido = $this->pdo->prepare("UPDATE cardapio_servido set ref_solida = :ref_solida,ref_liquida = :ref_liquida where id = :id");
+            $cardapio_servido->bindValue(":ref_solida", $ref_solida); 
+            $cardapio_servido->bindValue(":ref_liquida", $ref_liquida); 
+            $cardapio_servido->bindValue(":id", $id); 
+            $cardapio_servido->execute();
+        } catch (Exception $e) {
+            echo $e->getCode();
+        }
+    }
+
     public function registrar_lanche($data, $horario, $ref_solida, $ref_liquida) {
         if (!$this->get_cardapio($data, $horario)) {
             try {
@@ -58,7 +92,16 @@ Class Cardapio {
                 echo $e->getCode();
             }
         } else {
-            echo 'x';
+            $id = $this->get_cardapio($data, $horario)['id'];
+            $cardapio_atual = $this->get_cardapio_servido($id);
+            switch ($this->count_null($cardapio_atual)) {
+                case 7:
+                    $this->atualizar_cardapio_lanche($id, $ref_solida, $ref_liquida);
+                    break;
+                default:
+                    echo 'deu erro ' . $this->count_null($cardapio_atual);
+                    break;
+            }
         }
     }
 }
