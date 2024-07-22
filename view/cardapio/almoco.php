36f81252ec23138cb446eb30e3d9382bcd8bca90
Luana Moreira <luanamoreirafm@gmail.com>
2024-07-22 16:24:42 -0300

    att js e cardapio

--- view/cardapio/almoco.php
+++ view/cardapio/almoco.php
@@ -30,7 +30,7 @@
                     <label for="dataId">Cardápio referente ao dia</label>
                 </div>
                 <div class="linha">
-                    <input type="date" name="data" id="dataId">
+                    <input type="date" name="data" class="valor-input" data-campo="dataId">
                 </div>
             </div>
             <div class="grupo">
@@ -38,7 +38,7 @@
                     <label for="proteinaId">Qual a proteína servida?</label>
                 </div>
                 <div class="linha">
-                    <input type="text" name="proteina" id="proteinaId">
+                    <input type="text" name="proteina" class="valor-input" data-campo="proteinaId">
                 </div>
             </div>
             <div class="grupo">
@@ -46,7 +46,7 @@
                     <label for="carboidratoId">Qual o carboidrato servido?</label>
                 </div>
                 <div class="linha">
-                    <input type="text" name="carboidrato" id="carboidratoId">
+                    <input type="text" name="carboidrato" class="valor-input" data-campo="carboidratoId">
                 </div>
             </div>
             <div class="grupo">
@@ -54,7 +54,7 @@
                     <label for="verduraId">Qual a verdura servida?</label>
                 </div>
                 <div class="linha">
-                    <input type="text" name="verdura" id="verduraId">
+                    <input type="text" name="verdura" class="valor-input" data-campo="verduraId">
                 </div>
             </div>
             <div class="grupo">
@@ -62,7 +62,7 @@
                     <label for="legumeId">Qual o legume servido?</label>
                 </div>
                 <div class="linha">
-                    <input type="text" name="legume" id="legumeId">
+                    <input type="text" name="legume" class="valor-input" data-campo="legumeId">
                 </div>
             </div>
             <div class="grupo">
@@ -70,7 +70,7 @@
                     <label for="frutaId">Qual a fruta servida?</label>
                 </div>
                 <div class="linha">
-                    <input type="text" name="fruta" id="frutaId">
+                    <input type="text" name="fruta" class="valor-input" data-campo="frutaId">
                 </div>
             </div>
             <div class="grupo">
@@ -78,7 +78,7 @@
                     <label for="sucoId">Qual o suco servido?</label>
                 </div>
                 <div class="linha">
-                    <input type="text" name="suco" id="sucoId">
+                    <input type="text" name="suco" class="valor-input" data-campo="sucoId">
                 </div>
             </div>
             <div class="grupo">
@@ -87,6 +87,7 @@
                 </div>
                 <div class="linha">
                     <input type="text" name="sobremesa" id="sobremesaId">
+                    <input type="text" name="sobremesa" class="valor-input" data-campo="sobremesaId">
                 </div>
             </div>
             <div class="area-btn">
@@ -96,11 +97,11 @@
             <dialog id="confirmarId">
                 <div class="cabecalho">
                     <div class="linha-cab">
-                        <h3>Almoço do dia <span id="campoData"></span></h3>
+                        <h3>Almoço do dia <span class="campo-exibicao" data-campo="dataId"></span></h3>
                     </div>
                 </div>
                 <div class="linha">
-                    <p>Foi servido <span id="campoProteina"></span>, <span id="campoCarboidrato"></span>, <span id="campoVerdura"></span>, <span id="campoLegume"></span>, <span id="campoFruta"></span>, <span id="campoSuco"></span> e <span id="campoSobremesa"></span>.</p>
+                    <p>Foi servido <span class="campo-exibicao" data-campo="proteinaId"></span>, <span class="campo-exibicao" data-campo="carboidratoId"></span>, <span class="campo-exibicao" data-campo="verduraId"></span>, <span class="campo-exibicao" data-campo="legumeId"></span>, <span class="campo-exibicao" data-campo="frutaId"></span>, <span class="campo-exibicao" data-campo="sucoId"></span> e <span class="campo-exibicao" data-campo="sucoId"></span>.</p>
                 </div>
                 <div class="btns-dialog">
                     <input type="submit" class="btn mandar" id="enviarId" value="Confirmar" name="btn_almoco">
--- view/cardapio/lanche-manha.php
+++ view/cardapio/lanche-manha.php
@@ -25,12 +25,12 @@
             </div>
         </div>
         <form action="../../control/cardapio-lanche.php" method="post" autocomplete="off">
-            <div class="grupo">
+        <div class="grupo">
                 <div class="linha">
                     <label for="dataId">Cardápio referente ao dia</label>
                 </div>
                 <div class="linha">
-                    <input type="date" name="data" id="dataId">
+                    <input type="date" name="data" class="valor-input" data-campo="dataId">
                 </div>
             </div>
             <div class="grupo">
@@ -38,7 +38,7 @@
                     <label for="refSolidaId">O que foi servido para comer?</label>
                 </div>
                 <div class="linha">
-                    <input type="text" name="refSolida" id="refSolidaId">
+                    <input type="text" name="refSolida" class="valor-input" data-campo="refSolidaId">
                 </div>
             </div>
             <div class="grupo">
@@ -46,7 +46,7 @@
                     <label for="refLiquidaId">O que foi servido para beber?</label>
                 </div>
                 <div class="linha">
-                    <input type="text" name="refLiquida" id="refLiquidaId">
+                    <input type="text" name="refLiquida" class="valor-input" data-campo="refLiquidaId">
                 </div>
             </div>
             <div class="area-btn">
@@ -54,13 +54,13 @@
                 <input type="reset" class="btn limpar" value="limpar">
             </div>
             <dialog id="confirmarId">
-                <div class="cabecalho">
+            <div class="cabecalho">
                     <div class="linha-cab">
-                        <h3>Lanche da manhã do dia <span id="campoData"></span></h3>
+                        <h3>Lanche da manhã do dia <span class="campo-exibicao" data-campo="dataId"></span></h3>
                     </div>
                 </div>
                 <div class="linha">
-                    <p>Foi servido <span id="campoRefSol"></span> com <span id="campoRefLiq"></span>.</p>
+                    <p>Foi servido <span class="campo-exibicao" data-campo="refSolidaId"></span> com <span class="campo-exibicao" data-campo="refLiquidaId"></span>.</p>
                 </div>
                 <div class="btns-dialog">
                     <input type="submit" class="btn mandar" id="enviarId" value="Confirmar" name="btn_lanche">
