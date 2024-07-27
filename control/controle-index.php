e5236c5e5bceaa3368a2f8ac48acd07b2c9d0ff9
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2024-07-27 14:33:07 -0300

    verificação para votar
    - série
    - refeição
    - cardápio
    - manda para a página de erro

--- control/controle-index.php
+++ control/controle-index.php
@@ -4,16 +4,22 @@ require_once('../model/Cardapio.Class.php');
 if (isset($_POST['btn'])){
     $av = new Avaliacao;
     $refeicao = $_POST['horario'];
-    if ($av->pode_avaliar($refeicao)) {
+    if ($av->validar_horario($refeicao)[0]) {
         $avaliacao = $_POST['satisfacao'];
         $serie = $_POST['serie'];
-        $cardapio = new Cardapio;
-        if (!$cardapio->get_cardapio(date('Y-m-d'), $refeicao)) {
-            $id_cardapio = $cardapio->registrar_cardapio_nulo(date('Y-m-d'), $refeicao);
+        if ($serie ===  'null-serie') {
+            header('location: ../view/erro.php?err=serie');
         } else {
-            $id_cardapio = $cardapio->get_cardapio(date('Y-m-d'), $refeicao)['id'];
+            $cardapio = new Cardapio;
+            if (!$cardapio->get_cardapio(date('Y-m-d'), $refeicao)) {
+                $id_cardapio = $cardapio->registrar_cardapio_nulo(date('Y-m-d'), $refeicao);
+            } else {
+                $id_cardapio = $cardapio->get_cardapio(date('Y-m-d'), $refeicao)['id'];
+            }
+            $av->cadastrar_avaliacao($avaliacao, $serie, $refeicao, $id_cardapio);
         }
-        $av->cadastrar_avaliacao($avaliacao, $serie, $refeicao, $id_cardapio);
+    } else {
+        header('location: ../view/erro.php?err='.$av->validar_horario($refeicao)[1]);
     }
 }
 ?>
